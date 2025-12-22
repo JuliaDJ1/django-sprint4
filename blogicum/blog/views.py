a5cf25fa8fb2c3165b85ea79d03f4c6c2266be8b
Вадим Гусейнов <edvard.najt@yandex.ru>
2025-12-22 21:11:36 +0300

    04

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -3,20 +3,23 @@ from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from django.http import Http404
+from django.db.models import Count
 
-from .models import Post, Category, Comment
+from .models import Post, Category, Comment, User
 from .forms import PostForm, CommentForm
+
 
 def index(request):
     post_list = Post.objects.filter(
         is_published=True,
         category__is_published=True,
         pub_date__lte=timezone.now()
-    ).order_by('-pub_date')
+    ).annotate(count_comments=Count('comments')).order_by('-pub_date')
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'blog/index.html', {'page_obj': page_obj})
+
 
 def category_posts(request, category_slug):
     category = get_object_or_404(
@@ -28,37 +31,48 @@ def category_posts(request, category_slug):
         category=category,
         is_published=True,
         pub_date__lte=timezone.now()
-    ).order_by('-pub_date')
+    ).annotate(count_comments=Count('comments')).order_by('-pub_date')
     paginator = Paginator(post_list, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     return render(request, 'blog/category.html', {'category': category, 'page_obj': page_obj})
 
+
 def post_detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    if post.author != request.user and (not post.is_published or post.pub_date > timezone.now() or not post.category.is_published):
-        raise Http404
+    if not post.is_published or post.pub_date > timezone.now() or not post.category.is_published:
+        if post.author != request.user:
+            raise Http404
+    comments = post.comments.select_related('author').order_by('created_at')
     form = CommentForm()
-    comments = post.comments.all()
-    context = {'post': post, 'form': form, 'comments': comments}
+    context = {
+        'post': post,
+        'form': form,
+        'comments': comments
+    }
     return render(request, 'blog/detail.html', context)
 
+
 def profile(request, username):
-    from django.contrib.auth.models import User
-    profile = get_object_or_404(User, username=username)
-    posts = Post.objects.filter(author=profile)
-    if request.user != profile:
-        posts = posts.filter(is_published=True, pub_date__lte=timezone.now(), category__is_published=True)
+    profile_user = get_object_or_404(User, username=username)
+    posts = Post.objects.filter(author=profile_user).annotate(count_comments=Count('comments'))
+    if request.user != profile_user:
+        posts = posts.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True
+        )
     posts = posts.order_by('-pub_date')
     paginator = Paginator(posts, 10)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'profile': profile,
+        'profile': profile_user,
         'page_obj': page_obj,
-        'is_owner': request.user == profile,
+        'is_owner': request.user == profile_user,
     }
     return render(request, 'blog/profile.html', context)
+
 
 @login_required
 def create_post(request):
@@ -69,6 +83,7 @@ def create_post(request):
         post.save()
         return redirect('blog:profile', username=request.user.username)
     return render(request, 'blog/create.html', {'form': form})
+
 
 @login_required
 def edit_post(request, post_id):
@@ -81,6 +96,7 @@ def edit_post(request, post_id):
         return redirect('blog:post_detail', post_id=post_id)
     return render(request, 'blog/create.html', {'form': form, 'post': post})
 
+
 @login_required
 def add_comment(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
@@ -90,7 +106,9 @@ def add_comment(request, post_id):
         comment.author = request.user
         comment.post = post
         comment.save()
+        return redirect('blog:post_detail', post_id=post_id)
     return redirect('blog:post_detail', post_id=post_id)
+
 
 @login_required
 def edit_comment(request, post_id, comment_id):
@@ -101,24 +119,26 @@ def edit_comment(request, post_id, comment_id):
     if form.is_valid():
         form.save()
         return redirect('blog:post_detail', post_id=post_id)
-    return render(request, 'blog/comment.html', {'form': form, 'comment': comment})
+    return render(request, 'blog/comment.html', {'form': form, 'comment': comment, 'post': comment.post})
+
 
 @login_required
 def delete_post(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if post.author != request.user:
-        raise Http404
+        raise Http404("Нет доступа")
     if request.method == 'POST':
         post.delete()
         return redirect('blog:profile', username=request.user.username)
     return render(request, 'blog/detail.html', {'post': post})
 
+
 @login_required
 def delete_comment(request, post_id, comment_id):
     comment = get_object_or_404(Comment, pk=comment_id, post__pk=post_id)
     if comment.author != request.user:
-        raise Http404
+        raise Http404("Нет доступа")
     if request.method == 'POST':
         comment.delete()
         return redirect('blog:post_detail', post_id=post_id)
-    return render(request, 'blog/detail.html', {'post': comment.post, 'comment': comment})
+    return render(request, 'blog/detail.html', {'post': comment.post, 'comment_to_delete': comment})
--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -1,17 +1,22 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import handler403, handler404, handler500
-import users
+from django.conf import settings
+from django.conf.urls.static import static
+
+from .users import register
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', include('blog.urls', namespace='blog')),
     path('pages/', include('pages.urls')),
     path('auth/', include('django.contrib.auth.urls')),
-    path('auth/registration/', users.views.register, name='registration'),
-    path('profile/edit/', users.views.edit_profile, name='edit_profile'),
+    path('auth/registration/', register, name='registration'),
 ]
 
 handler403 = 'pages.views.csrf_failure'
 handler404 = 'pages.views.page_not_found'
 handler500 = 'pages.views.server_error'
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
